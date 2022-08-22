0a88a8240614e7e2f4b8fa1f4b47f51f5c7bac83
yeojoo1224 <yeojoo1224@naver.com>
2022-08-22 11:23:08 +0900

    initial

---
+++ src/main/java/com/codestates/TodoApphomework/TodoAppHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.TodoApphomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppHomeworkApplication.class, args);
+	}
+
+}
